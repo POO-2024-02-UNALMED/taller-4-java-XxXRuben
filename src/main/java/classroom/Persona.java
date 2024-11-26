86148574a2788c3810d236b366012bbbf5328e19
XxXRuben <rdazas@unal.edu.co>
2024-11-26 12:17:41 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -1,25 +1,23 @@
 package classroom;
 
 public class Persona {
+    public long cedula;
+    public  String nombre;
+    public static int totalPersonas;
 
-    final long cedula;
-    String nombre;
-    static int totalPersonas;
-    
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
-    public Persona(long cedula, String nombre) {
-        this.cedula = cedula;
-        this.nombre = nombre;
+    
+    public Persona() {
+        this.cedula = 0;
         totalPersonas++;
     }
-    
-    public Persona(String nombre, long cedula) {
-        this.cedula = cedula;
+
+    public Persona(String nombre) {
         this.nombre = nombre;
+        this.cedula = 1; 
         totalPersonas++;
     }
 
@@ -29,20 +27,36 @@ public class Persona {
         totalPersonas++;
     }
 
-    public Persona(String nombre) {
-        this.nombre = "";
+
+    public Persona( String nombre, long cedula) {
+        this.cedula = cedula;
+        this.nombre = nombre;
         totalPersonas++;
     }
-    
+
+
+    public Persona(long cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
+    }
+
+
     public long getCedula() {
         return cedula;
     }
+
 
     public String getNombre() {
         return nombre;
     }
 
+    
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public void setCedula(long cedula) {
+        this.cedula = cedula;
     }
 }
